9bb5af33266556633623b17647121d1af5bc752a
TheCreatorFr <MrBrondinNicolas@gmail.com>
2016-08-17 10:07:08 +0200

    [PLUGINS] transformedthe static console into a plugins platform

---
+++ js/plugins/core/plugin.js
@@ -0,0 +1,61 @@
+var cmd_clear = function() {
+    $('.old').remove();
+    return '';
+};
+
+function clean(s) {
+    var r = s.toLowerCase();
+    return r.replace(new RegExp("[àáâãäå]", 'g'),"a")
+            .replace(new RegExp("æ", 'g'),"ae")
+            .replace(new RegExp("ç", 'g'),"c")
+            .replace(new RegExp("[èéêë]", 'g'),"e")
+            .replace(new RegExp("[ìíîï]", 'g'),"i")
+            .replace(new RegExp("ñ", 'g'),"n")                    
+            .replace(new RegExp("[òóôõö]", 'g'),"o")
+            .replace(new RegExp("œ", 'g'),"oe")
+            .replace(new RegExp("[ùúûü]", 'g'),"u")
+            .replace(new RegExp("[ýÿ]", 'g'),"y");
+};
+
+var cmd_lang = function(text) {
+    if(clean(text).indexOf(clean(" fr"))!=-1){
+        lang = 'fr';
+        return 'langue changée';
+    } else if(clean(text).indexOf(clean(" en"))!=-1) {
+        lang = 'en';
+        return 'language changed';
+    } else {
+        return 'language not found';
+    }
+};
+
+
+var cmd_help = function() {
+    var html = '<table class="old">';
+    for(item of plugins) {
+        html += "<tr><td>"+item.manual[lang].usage+"</td><td>"+item.manual[lang].description+"</td></tr>";
+    }
+    return html+"</table>";
+};
+
+plugins = plugins.concat([
+{ name:"clear", 
+      callback:cmd_clear,
+      manual:{
+        en:{ usage:'clear', description:'clear the console'},
+        fr:{ usage:'clear', description:'efface la console'}
+      }
+},{ name:"help", 
+      callback:cmd_help,
+      manual:{
+        en:{ usage:'help', description: 'list all the commands available in the console'},
+        fr:{ usage:'help', description: 'liste toutes les commandes disponibles'}
+      }
+    },
+    { name:"lang", 
+      callback:cmd_lang,
+      manual:{
+        en:{ usage:"lang 'fr'", description:'change the language'},
+        fr:{ usage:"lang 'en'", description:'change la langue'}
+      }
+    }]);
---
+++ js/plugins/resume/plugin.js
@@ -0,0 +1,52 @@
+var cat = { 
+    en:['Profile','Education','Competences','Experience','Hobbies'],
+    fr:['Profil','Etudes','Compétences','Expérience','Hobbies']
+};
+function readfile(filename) {
+    var info;
+    $.ajax({ type: 'GET', async: false, url: filename, dataType: 'text', success:function(data){
+        info=data;
+    }});
+    return info;
+}
+
+var cmd_ls = function() {
+    var html = '<table class="old">';
+    for(item of cat[lang]) {
+        html += "<tr><td>"+item+"</td></tr>";
+    }
+    return html+"</table>";
+};
+
+
+var cmd_cat = function(text) {
+    var cat_string = clean(text);
+    for(c of cat[lang]) {
+        if(cat_string.indexOf(clean(c))!=-1) {
+            content = readfile(clean(c)+'_'+lang+'.txt');
+            if(content)
+                return content;
+        }
+    }
+    
+    if(lang =='fr') {
+        return 'catégorie inconnue';
+    } else {
+        return 'category not found';
+    }
+};
+
+plugins = plugins.concat([    { name:"ls", 
+      callback:cmd_ls,
+      manual:{
+        en:{ usage:'ls', description:'show all the categories of my résumé'},
+        fr:{ usage:'ls', description:'affiche toutes les catégories de mon cv'}
+      }
+    },
+    { name:"cat", 
+      callback:cmd_cat,
+      manual:{
+        en:{ usage:"cat 'category'", description:'display the content of the selected category'},
+        fr:{ usage:"cat 'catégorie'", description:'affiche le contenu de la catégorie sélectionnée'}
+      }
+    }]);
